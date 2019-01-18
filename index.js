5bdedcffa1b949103db62769463aea4582fff3d1
cjbrock <corinna@flatironschool.com>
2019-01-18 08:08:58 -0500

    Rough draft finished

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
-const testVar = {}
+// Strings
+const farmAnimals = 'cow horse sheep pig chicken'
+// 1. Use destructuring to assign appropriate varibles based on the sounds animals make.
+const [moo, neigh, baa, oink, cluck] = 'cow horse sheep pig chicken'.split(" ")
 
-function testFunc() {
-  return "hi"
-}
+// 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
+const [Bessie, , Dolly, Babe, Little] = 'cow horse sheep pig chicken'.split(" ")
+
+// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
+const [black_and_white, , black, pink] = 'cow horse sheep pig chicken'.split(" ")
+
+// Arrays
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+// 4. Use destructuring to assign appropriate varibles using the color names.
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+
+// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+const [r, o, y, g, b, , v] = colors
+
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const [ , , , , , indg, ] = colors
+
+// Objects
+const muppet = {
+  name: 'Kermit',
+  color: 'green',
+  song: 'The Rainbow Connection',
+  job: 'Host of The Muppet Show',
+  partner: 'Miss Piggy'
+};
+// 7. Use destructuring to assign all appropriate variables using the keys as the variable names
+const { name, color, song, job, partner} = muppet
+
+// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const k_muppet = {
+  k_name: 'Kermit',
+  k_color: 'green',
+  k_album: {
+    the_muppet_movie: {
+      song_1: 'Rainbow Connection',
+      song_2: 'Moving Right Along',
+      song_3: 'Never Before, Never Again',
+      song_4: 'I Hope That Something Better Comes Along',
+    },
+  },
+  k_job: 'Host of The Muppet Show',
+  k_partner: 'Miss Piggy'
+};
+
+const { song_2, song_4} = muppet.k_album_the_muppet_movie
+const { k_color, k_job, k_partner } = muppet
